5f127e27c9add8e075fc59297d2d284d2b837c7b
Mateus Bonet <mateusbonet@yahoo.com.br>
2021-07-14 21:55:33 -0300

    Doc: Template limpo

--- app.js
+++ app.js
@@ -5,30 +5,10 @@ app.get('/', function (req, res) {
   res.send({"teste": 'Hello World!' , teste: 123})
 })
 
-app.get('/teste/:valor&:tipo', function (req, res) {
-  
-  const bwipjs = require('bwip-js');
-
-        bwipjs.toBuffer({
-            bcid: 'qrcode',
-            text: req.params.valor,
-            scale: 3,
-            height: 10,
-            includetext: true,
-            textxalign: 'center'
-        }, function(error, buffer) {
-            if(error) {
-                reject(error)
-            } else {
-                let gifBase64 = `data:image/gif;base64,${buffer.toString('base64')}`
-                res.send({"gifBase64": gifBase64})
-                
-            }
-        });
-  
-  /*res.send({"teste": req.params.valor , teste2: req.params.tipo})*/
+app.get('/consultaprodutos', function (req, res) {
+  res.send({"teste": 'Hello World!' , teste: { "cd": 12 , "descr": "produto teste"} })
 })
 
 app.listen(80, function () {
-  console.log('Servidor da NASA -> porta: 80!')
+  console.log('API template iniciado porta: 80')
 })
